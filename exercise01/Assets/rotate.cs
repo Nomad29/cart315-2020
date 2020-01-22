f516f313709fa97912e9235b446a5f971571d324
Nomad29 <alexandramelancon@outlook.com>
2020-01-22 12:11:11 -0500

    Week 3 in progress in class exercise

---
+++ exercise01/Assets/rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float rotate_speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      if (Input.GetButton("Fire1"))
+      {
+        this.transform.Rotate(0.0f, rotate_speed, 0.0f);
+      }
+    }
+}
